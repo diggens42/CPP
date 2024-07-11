a6c47a42746847d8925b7927ed98a25e1596d76f
diggens42 <frederic.marcel.wahl@gmail.com>
2024-07-11 19:30:19 +0200

    added phonebook class

--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: fwahl <fwahl@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 16:36:04 by fwahl             #+#    #+#             */
-/*   Updated: 2024/07/11 17:39:27 by fwahl            ###   ########.fr       */
+/*   Updated: 2024/07/11 19:28:42 by fwahl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,11 +41,25 @@ class Contact
 
 		void get_contact() const
 		{
-			std::cout << "First Name: " << get_firstname() << std::endl;
-			std::cout << "Last Name: " << get_lastname() << std::endl;
-			std::cout << "Nickname: " << get_nickname() << std::endl;
-			std::cout << "Phonenumber: " << get_phonenumber() << std::endl;
-			std::cout << "Darkest Secret: " << get_darkestsecret() << std::endl;
+			std::cout
+				<< "First Name: " << get_firstname() <<
+			std::endl;
+
+			std::cout
+				<< "Last Name: " << get_lastname() <<
+			std::endl;
+
+			std::cout
+				<< "Nickname: " << get_nickname() <<
+			std::endl;
+
+			std::cout
+				<< "Phonenumber: " << get_phonenumber() <<
+			std::endl;
+
+			std::cout
+				<< "Darkest Secret: " << get_darkestsecret() <<
+			std::endl;
 		}
 };
 
@@ -55,4 +69,70 @@ class PhoneBook
 		Contact	contacts[8];
 		int		i;
 		int		oldest;
+
+		std::string truncate(const std::string& string) const
+		{
+			if (string.length() > 10)
+				return (string.substr(0, 9) + ".");
+			return (string);
+		}
+
+	public:
+		PhoneBook() : i(0), oldest(0) {}
+
+		void add_contact(const Contact& contact)
+		{
+			if (i < 8)
+			{
+				contacts[i] = contact;
+				i++;
+			}
+			else
+			{
+				contacts[oldest] = contact;
+				oldest = (oldest + 1) % 8;
+			}
+		}
+
+		void search_contact() const
+		{
+			if (i == 0)
+			{
+				std::cout
+					<< "No contacts in phonebook" <<
+				std::endl;
+				return ;
+			}
+			std::cout
+				<< std::setw(10) << "Index" << "|"
+				<< std::setw(10) << "First Name" << "|"
+				<< std::setw(10) << "Last Name" << "|"
+				<< std::setw(10) << "Nickname" <<
+			std::endl;
+
+			int	j = 0;
+			while (j < i)
+			{
+				std::cout
+					<< std::setw(10) << j << "|"
+					<< std::setw(10) << truncate(contacts[j].get_firstname()) << "|"
+					<< std::setw(10) << truncate(contacts[j].get_lastname()) << "|"
+					<< std::setw(10) << truncate(contacts[j].get_nickname()) << "|" <<
+				std::endl;
+			}
+
+			int	idx;
+			std::cout << "Enter index of contact to display ";
+			std::cin >> idx;
+
+			if (idx >= 0 && idx < i)
+				contacts[idx].get_contact();
+			else
+			{
+				std::cout
+					<< "index no bueno" <<
+				std::endl;
+
+			}
+		}
 };
--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -6,7 +6,13 @@
 /*   By: fwahl <fwahl@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 17:11:14 by fwahl             #+#    #+#             */
-/*   Updated: 2024/05/17 17:11:15 by fwahl            ###   ########.fr       */
+/*   Updated: 2024/07/11 19:29:57 by fwahl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "PhoneBook.hpp"
+
+int	main()
+{
+	PhoneBook phonebook;
+}
