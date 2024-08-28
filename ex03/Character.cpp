38031219a556c7e0e6938440a51adf23fe274f09
diggens42 <frederic.marcel.wahl@gmail.com>
2024-08-28 20:19:29 +0200

    character class done

--- ex03/Character.cpp
+++ ex03/Character.cpp
@@ -4,22 +4,86 @@
 Character::Character()
 {
 	std::cout << "Character default constructor called" << std::endl;
+	int	i = 0;
+	while (i < _slots)
+	{
+		_inventory[i] = NULL;
+		i++;
+	}
+	i = 0;
+	while (i < _storage_slots)
+	{
+		_storage[i] = NULL;
+		i++;
+	}
 }
 
 Character::Character(std::string const & name) : _name(name)
 {
 	std::cout << "Character named constructor called" << std::endl;
+	int	i = 0;
+	while (i < _slots)
+	{
+		_inventory[i] = NULL;
+		i++;
+	}
+	i = 0;
+	while (i < _storage_slots)
+	{
+		_storage[i] = NULL;
+		i++;
+	}
 }
 
 Character::Character(const Character &other)
 {
 	std::cout << "Character copy constructor called" << std::endl;
+	int	i = 0;
+	while (i < _slots)
+	{
+		_inventory[i] = NULL;
+		i++;
+	}
+	i = 0;
+	while (i < _storage_slots)
+	{
+		_storage[i] = NULL;
+		i++;
+	}
+	*this = other;
 }
+
 
 Character& Character::operator=(const Character &other)
 {
 	if (this != &other)
 	{
+		this->_name = other.getName();
+		this->_storage_idx = other._storage_idx;
+		int i = 0;
+		while (i < _slots)
+		{
+			if (_inventory[i] != NULL)
+			{
+				delete _inventory[i];
+				_inventory[i] = NULL;
+			}
+			if (other._inventory[i] != NULL)
+				_inventory[i] = other._inventory[i]->clone();
+			i++;
+		}
+		i = 0;
+		while (i < _storage_slots)
+		{
+			if (_storage[i] != NULL)
+			{
+				delete _storage[i];
+				_storage[i] = NULL;
+			}
+			if (other._storage[i] != NULL)
+				_storage[i] = other._storage[i]->clone();
+			i++;
+		}
 		std::cout << "Character copy assignment operator called" << std::endl;
 	}
 	return *this;
@@ -27,6 +91,20 @@ Character& Character::operator=(const Character &other)
 
 Character::~Character()
 {
+	int i = 0;
+	while (i < _slots)
+	{
+		if (_inventory[i] != NULL)
+			delete _inventory[i];
+		i++;
+	}
+	i = 0;
+	while (i < _storage_slots)
+	{
+		if (_storage[i] != NULL)
+			delete _storage[i];
+		i++;
+	}
 	std::cout << "Character destructor called" << std::endl;
 }
 
