ecb6c15208d66239070a2c3683e6d47860de8934
diggens42 <frederic.marcel.wahl@gmail.com>
2024-09-30 18:25:02 +0200

    <optional> instead of bools

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -26,101 +26,69 @@ ScalarConverter::~ScalarConverter()
 }
 
 
-static bool	isCharLiteral(const std::string& lit)
+bool	ScalarConverter::isCharLiteral(const std::string& lit)
 {
 	if (lit.length() == 1 && !std::isdigit(lit[0]) && std::isprint(lit[0]))
 		return (true);
 	return (false);
 }
-static bool	isIntLiteral(const std::string& lit)
+std::optional<long>	ScalarConverter::isIntOpt(const std::string& lit)
 {
-	char*	pos;
-	strtol(lit.c_str(), &pos, 10);
-	return (*pos == '\0');
+	char*		pos;
+	long		intie;
+	intie = strtol(lit.c_str(), &pos, 10);
+	if (*pos == '\0' && intie <= std::numeric_limits<int>::max() && intie >= std::numeric_limits<int>::min())
+		return (intie);
+	return (std::nullopt);
 }
-static bool	isFloatLiteral(const std::string& lit)
+std::optional<float>	ScalarConverter::isFloatOpt(const std::string& lit)
 {
 	char*	pos;
 	float	floatie;
 
 	floatie = std::strtof(lit.c_str(), &pos);
 	if (*pos == 'f' && *(pos + 1) == '\0')
-		return (true);
-	return (false);
-
-	// int		start = 0;
-	// bool	decimalPoint = false;
-	// bool	integralPart = false;
-	// bool	fractionalPart = false;
-
-	// if (lit[0] == '+' || lit[0] == '-')
-	// 	start = 1;
-
-	// int		i = start;
-	// while (i < lit.length())
-	// {
-	// 	if (std::isdigit(lit[i]))
-	// 	{
-	// 		if (!decimalPoint)
-	// 			integralPart = true;
-	// 		else
-	// 			fractionalPart = true;
-	// 	}
-	// 	else if (lit[i] == '.')
-	// 	{
-	// 		if (decimalPoint)
-	// 			return false;
-	// 		decimalPoint = true;
-	// 	}
-	// 	else if (lit[i] == 'f')
-	// 		return (i == lit.length() - 1 && decimalPoint && (integralPart || fractionalPart));
-	// 	else
-	// 		return false;
-	// 	i++;
-	// }
-	// return false;
+		return (floatie);
+	return (std::nullopt);
 }
-static bool	isDoubleLiteral(const std::string& lit)
+std::optional<double>	ScalarConverter::isDoubleOpt(const std::string& lit)
 {
 	char*	pos;
 	double	michael_double;
 
 	michael_double = strtod(lit.c_str(), &pos);
 	if (*pos == '\0')
-		return (true);
-	return (false);
-
-	// int		start = 0;
-	// bool	decimalPoint = false;
-	// bool	integralPart = false;
-	// bool	fractionalPart = false;
-
-	// if (lit[0] == '+' || lit[0] == '-')
-	// 	start = 1;
-
-	// int		i = start;
-	// while (i < lit.length())
-	// {
-	// 	if (std::isdigit(lit[i]))
-	// 	{
-	// 		if (!decimalPoint)
-	// 			integralPart = true;
-	// 		else
-	// 			fractionalPart = true;
-	// 	}
-	// 	else if (lit[i] == '.')
-	// 	{
-	// 		if (decimalPoint)
-	// 			return false;
-	// 		decimalPoint = true;
-	// 	}
-	// 	else if (lit[i] == '\0')
-	// 		return (decimalPoint && (integralPart || fractionalPart));
-	// 	else
-	// 		return false;
-	// 	i++;
-	// }
-	// return false;
+		return (michael_double);
+	return (std::nullopt);
 }
 
+void	ScalarConverter::convert(const std::string& lit)
+{
+	if (isCharLiteral(lit) == true)
+	{
+		//add print
+	}
 
+	std::optional<long>		intOpt = isIntOpt(lit);
+	if (intOpt.has_value())
+	{
+		long intie = intOpt.value();
+		//add print
+	}
+
+	std::optional<float>	floatOpt = isFloatOpt(lit);
+	if (floatOpt.has_value())
+	{
+		float	floatie = floatOpt.value();
+		//add print
+	}
+
+	std::optional<double>	doubleOpt = isDoubleOpt(lit);
+	if (doubleOpt.has_value())
+	{
+		double	michael_double = doubleOpt.value();
+		//add print
+	}
+
+	std::cout << "Invalid input" << std::endl;
+}
--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -10,21 +10,24 @@
 #include <limits>
 #include <cmath>
 #include <iomanip>
+#include <optional>
 
 class ScalarConverter
 {
 	public:
 		~ScalarConverter();
+		static void	convert(const std::string& lit);
 
 	private:
 		ScalarConverter();
 		ScalarConverter(const ScalarConverter &other);
 		ScalarConverter& operator=(const ScalarConverter &other);
 
-		static bool	isCharLiteral(const std::string& lit);
-		static bool	isIntLiteral(const std::string& lit);
-		static bool	isFloatLiteral(const std::string& lit);
-		static bool	isDoubleLiteral(const std::string& lit);
+		static bool						isCharLiteral(const std::string& lit);
+		static std::optional<long>		isIntOpt(const std::string& lit);
+		static std::optional<float>		isFloatOpt(const std::string& lit);
+		static std::optional<double>	isDoubleOpt(const std::string& lit);
+
 };
 
 #endif // SCALARCONVERTER_H
