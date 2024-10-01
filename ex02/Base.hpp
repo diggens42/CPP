80b52850810cc874c9543870fcaa6ea2fb7f9c83
diggens42 <frederic.marcel.wahl@gmail.com>
2024-10-01 19:48:51 +0200

    files created ex02

---
+++ ex02/Base.hpp
@@ -0,0 +1,15 @@
+#ifndef BASE_H
+#define BASE_H
+
+#define	GREY  "\033[38;5;246m"
+#define	RESET "\033[0m"
+
+#include <iostream>
+
+class Base
+{
+	public:
+		virtual ~Base();
+};
+
+#endif // BASE_H
