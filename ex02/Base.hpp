971b4c145bfe49f3cb8d4fad7747f7c757388fa6
diggens42 <frederic.marcel.wahl@gmail.com>
2024-10-01 20:10:03 +0200

    added tests ex02

--- ex02/Base.hpp
+++ ex02/Base.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Base.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fwahl <fwahl@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/01 20:05:08 by fwahl             #+#    #+#             */
+/*   Updated: 2024/10/01 20:05:15 by fwahl            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef BASE_H
 #define BASE_H
 
@@ -5,11 +17,19 @@
 #define	RESET "\033[0m"
 
 #include <iostream>
+#include <string>
+#include <random>
+
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
 
 class Base
 {
 	public:
-		virtual ~Base();
+		virtual	~Base();
 };
-
+Base*	generate(void);
+void	identify(Base* p);
+void	identify(Base& p);
 #endif // BASE_H
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fwahl <fwahl@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 19:47:55 by fwahl             #+#    #+#             */
-/*   Updated: 2024/10/01 19:48:15 by fwahl            ###   ########.fr       */
+/*   Updated: 2024/10/01 20:09:48 by fwahl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,3 +14,68 @@
 #include "A.hpp"
 #include "B.hpp"
 #include "C.hpp"
+
+int main(void)
+{
+	{
+	std::cout << std::endl;
+	std::cout << "----------------------------" << std::endl;
+	std::cout << "           Test 1           " << std::endl;
+	std::cout << "----------------------------" << std::endl;
+	std::cout << std::endl;
+	Base* veryrandommuchwow = generate();
+	identify(veryrandommuchwow);
+	identify(*veryrandommuchwow);
+
+	delete (veryrandommuchwow);
+	}
+	{
+	std::cout << std::endl;
+	std::cout << "----------------------------" << std::endl;
+	std::cout << "           Test 2           " << std::endl;
+	std::cout << "----------------------------" << std::endl;
+	std::cout << std::endl;
+	Base* veryrandommuchwow = generate();
+	identify(veryrandommuchwow);
+	identify(*veryrandommuchwow);
+
+	delete (veryrandommuchwow);
+	}
+	{
+	std::cout << std::endl;
+	std::cout << "----------------------------" << std::endl;
+	std::cout << "           Test 3           " << std::endl;
+	std::cout << "----------------------------" << std::endl;
+	std::cout << std::endl;
+	Base* veryrandommuchwow = generate();
+	identify(veryrandommuchwow);
+	identify(*veryrandommuchwow);
+
+	delete (veryrandommuchwow);
+	}
+	{
+	std::cout << std::endl;
+	std::cout << "----------------------------" << std::endl;
+	std::cout << "           Test 4           " << std::endl;
+	std::cout << "----------------------------" << std::endl;
+	std::cout << std::endl;
+	Base* veryrandommuchwow = generate();
+	identify(veryrandommuchwow);
+	identify(*veryrandommuchwow);
+
+	delete (veryrandommuchwow);
+	}
+	{
+	std::cout << std::endl;
+	std::cout << "----------------------------" << std::endl;
+	std::cout << "           Test 5           " << std::endl;
+	std::cout << "----------------------------" << std::endl;
+	std::cout << std::endl;
+	Base* veryrandommuchwow = generate();
+	identify(veryrandommuchwow);
+	identify(*veryrandommuchwow);
+
+	delete (veryrandommuchwow);
+	}
+	return (0);
+}
