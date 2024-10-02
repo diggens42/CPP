9474e25163f4df0072262b8dbb117e089605e4a9
diggens42 <frederic.marcel.wahl@gmail.com>
2024-10-02 18:19:47 +0200

    ex01 implementation & tests

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,15 +6,38 @@
 /*   By: fwahl <fwahl@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 16:15:08 by fwahl             #+#    #+#             */
-/*   Updated: 2024/10/02 16:15:33 by fwahl            ###   ########.fr       */
+/*   Updated: 2024/10/02 17:47:07 by fwahl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 #define ITER_HPP
 
+#include <iostream>
 
+template<typename T, typename Func>
+void	iter(T* array, size_t len, Func ft)
+{
+	size_t	i = 0;
+	while (i < len)
+	{
+		ft(array[i]);
+		i++;
+	}
 
+}
+
+template<typename T>
+void	printEntry(T const& entry)
+{
+	std::cout << entry << "   ";
+}
+
+template<typename T>
+void	squareSomething(T& entry)
+{
+	entry = entry * entry;
+}
 
 
 #endif // ITER_HPP
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fwahl <fwahl@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 16:14:41 by fwahl             #+#    #+#             */
-/*   Updated: 2024/10/02 16:16:32 by fwahl            ###   ########.fr       */
+/*   Updated: 2024/10/02 18:16:19 by fwahl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,48 @@
 
 int main(void)
 {
+	std::cout << std::endl;
+	std::cout << "----------------------------" << std::endl;
+	std::cout << "          Int array         " << std::endl;
+	std::cout << "----------------------------" << std::endl;
+	std::cout << std::endl;
+
+	int		int_array[] = {15, 22, 33, 55, 66, 88};
+	size_t	int_array_len = sizeof(int_array)/ sizeof(int);
+	std::cout << "Original values: " << std::endl;
+	iter(int_array, int_array_len, printEntry<int>);
+	std::cout << std::endl;
+	std::cout << "Squared values:  " << std::endl;
+	iter(int_array, int_array_len, squareSomething<int>);
+	iter(int_array, int_array_len, printEntry<int>);
+
+
+	std::cout << std::endl;
+	std::cout << "----------------------------" << std::endl;
+	std::cout << "        Double array        " << std::endl;
+	std::cout << "----------------------------" << std::endl;
+	std::cout << std::endl;
+
+	double	double_array[] = {2.4, 3.6, 4.5, 5.6, 1.8};
+	size_t	double_array_len = sizeof(double_array)/ sizeof(double);
+	std::cout << "Original values: " << std::endl;
+	iter(double_array, double_array_len, printEntry<double>);
+	std::cout << std::endl;
+	std::cout << "Squared values:  " << std::endl;
+	iter(double_array, double_array_len, squareSomething<double>);
+	iter(double_array, double_array_len, printEntry<double>);
+
+
+	std::cout << std::endl;
+	std::cout << "----------------------------" << std::endl;
+	std::cout << "        String array        " << std::endl;
+	std::cout << "----------------------------" << std::endl;
+	std::cout << std::endl;
+
+	std::string	str_array[] = {"templates", "are", "kinda", "cool"};
+	size_t		str_array_len = sizeof(str_array)/ sizeof(std::string);
+	std::cout << "Printing string array: " << std::endl;
+	iter(str_array, str_array_len, printEntry<std::string>);
 
 	return (0);
 }
