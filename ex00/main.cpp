26192cb720135965ca6a70c71c2a898357501b14
diggens42 <frederic.marcel.wahl@gmail.com>
2024-10-02 15:59:25 +0200

    tests from subject

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,12 +6,28 @@
 /*   By: fwahl <fwahl@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 14:30:55 by fwahl             #+#    #+#             */
-/*   Updated: 2024/10/02 14:31:11 by fwahl            ###   ########.fr       */
+/*   Updated: 2024/10/02 15:58:50 by fwahl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(void)
-{
+#include "whatever.hpp"
 
-	return (0);
+int main( void )
+{
+	int a = 2;
+	int b = 3;
+
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
 }
