a9a47f02d259c209d70e008d07118717b1f69d89
diggens42 <frederic.marcel.wahl@gmail.com>
2024-10-02 20:32:24 +0200

    implemented most ft's ex02

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -6,12 +6,16 @@
 /*   By: fwahl <fwahl@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 18:22:49 by fwahl             #+#    #+#             */
-/*   Updated: 2024/10/02 18:59:29 by fwahl            ###   ########.fr       */
+/*   Updated: 2024/10/02 20:30:53 by fwahl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARRAY_HPP
-#define ARRAY
+#define ARRAY_HPP
+
+#include <cstddef>
+#include <exception>
+#include <stdexcept>
 
 template <typename T>
 class Array
@@ -20,16 +24,16 @@ class Array
 		Array();
 		explicit Array(unsigned int n);
 		Array(const Array& other);
-		Array&	operator=(const Array& other);
 		~Array();
 
-		T&	operator[](unsigned int idx);
-		const T& operator[](unsigned int idx) const;
+		Array&		operator=(const Array& other);
+		T&			operator[](unsigned int idx);
+		const T&	operator[](unsigned int idx) const;
 
 		unsigned int	size() const;
 	private:
-		T*				entries;
-		unsigned int	count;
+		T*				_entries;
+		unsigned int	_n;
 
 };
 
