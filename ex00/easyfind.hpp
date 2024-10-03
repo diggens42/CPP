86e530209a32e74a177ff01beee1247fbc31bd16
diggens42 <frederic.marcel.wahl@gmail.com>
2024-10-03 23:29:25 +0200

    basic layout ex01

---
+++ ex00/easyfind.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fwahl <fwahl@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/03 23:10:33 by fwahl             #+#    #+#             */
+/*   Updated: 2024/10/03 23:15:35 by fwahl            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+template <typename T>
+typename	T::iterator easyfind(T& haystack, int needle)
+{
+	
+}
+
+
+#endif // EASYFIND_HPP
