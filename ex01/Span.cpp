9cd4353c2a07b7fbd8d232a637049d31db19b4eb
diggens42 <frederic.marcel.wahl@gmail.com>
2024-10-04 01:44:52 +0200

    started on ex01

--- ex01/Span.cpp
+++ ex01/Span.cpp
@@ -6,20 +6,25 @@
 /*   By: fwahl <fwahl@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 01:14:08 by fwahl             #+#    #+#             */
-/*   Updated: 2024/10/04 01:14:09 by fwahl            ###   ########.fr       */
+/*   Updated: 2024/10/04 01:43:55 by fwahl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
 
-Span::Span()
+Span::Span() : _max(0), _nums(0)
 {
-	std::cout << GREY << "Default constructor called" << RESET << std::endl;
+	std::cout << GREY << "Span default constructor called" << RESET << std::endl;
+}
+
+Span::Span(unsigned int N) : _max(N)
+{
+	std::cout << GREY << "Span param constructor called" << RESET << std::endl;
 }
 
 Span::Span(const Span &other)
 {
-	std::cout << GREY << "Copy constructor called" << RESET << std::endl;
+	std::cout << GREY << "Span copy constructor called" << RESET << std::endl;
 	*this = other;
 }
 
@@ -27,12 +32,19 @@ Span& Span::operator=(const Span &other)
 {
 	if (this != &other)
 	{
-		std::cout << GREY << "Copy assignment operator called" << RESET << std::endl;
+		std::cout << GREY << "Span copy assignment operator called" << RESET << std::endl;
 	}
 	return *this;
 }
 
 Span::~Span()
 {
-	std::cout << GREY << "Destructor called" << RESET << std::endl;
+	std::cout << GREY << "Span destructor called" << RESET << std::endl;
+}
+
+void	Span::addNumber(int num)
+{
+	if (_nums.size() >= _max)
+		throw (SpanIsFullException());
+	_nums.push_back(num);
 }
--- ex01/Span.hpp
+++ ex01/Span.hpp
@@ -6,7 +6,7 @@
 /*   By: fwahl <fwahl@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 01:14:14 by fwahl             #+#    #+#             */
-/*   Updated: 2024/10/04 01:14:15 by fwahl            ###   ########.fr       */
+/*   Updated: 2024/10/04 01:44:35 by fwahl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,36 @@
 #define	GREY  "\033[38;5;246m"
 #define	RESET "\033[0m"
 
+#include <exception>
 #include <iostream>
+#include <deque>
+
+class SpanIsFullException : public std::exception
+{
+	public:
+		const char*	what() const noexcept override
+		{
+			return("Span is full");
+		}
+};
 
 class Span
 {
 	public:
 		Span();
+		Span(unsigned int N);
 		Span(const Span &other);
 		Span& operator=(const Span &other);
 		~Span();
 
-	private:
+		void	addNumber(int num);
 
+		template<typename Iter>
+		void	addNumbers(Iter begin, Iter end);
+
+	private:
+		unsigned int	_max;
+		std::deque<int>	_nums;
 };
 
 #endif // SPAN_H
